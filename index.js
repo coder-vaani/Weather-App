7a6df023026327ac246007f9183d1314f3cca2e5
Ashish Ranjan <aashishvaani@gmail.com>
2023-05-17 13:02:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const apikey = "b8292b7519e9925f611faab6b9aba493";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const search = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+
+    if (response.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    } else {
+        let data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
+        }
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+    }
+
+
+}
+
+//getting the city name from the search box
+searchButton.addEventListener("click", () => {
+    checkWeather(search.value);
+})
